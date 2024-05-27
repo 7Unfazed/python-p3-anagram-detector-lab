b556996470931c2c1fe168179b1e24f8f6d5e59e
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-05-27 08:52:24 +0300

    creates class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+    
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if ''.join(sorted(anagram)) == self.sorted_word]
+
+
